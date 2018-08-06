8279ec878a64d534b6ab7b9117e9be730041f0cb
Niederb <thomas@niederberger.ch>
2018-08-06 19:33:45 +0200

    Just generate some kind of image

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ray_tracer
 Simple ray tracer implemented in Rust
+This implementation is based on the book "Ray Tracing in One Weekend" by Peter Shirley
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let height = 200;
+    let width = 300;
+
+    let mut f2 = File::create("image.txt").expect("Unable to create file");
+    write!(f2, "P3\n{} {}\n255\n", width, height).expect("cout not write to file");
+
+    for y in 0..height {
+        for x in 0..width {
+            let r = 256 * x / width;
+            let g = 256 * y / height;
+            let b = 256 * (x + y) / (width + height);
+            write!(f2, "{} {} {}\n", r, g, b).expect("could not write to file");
+        }
+    }
 }
